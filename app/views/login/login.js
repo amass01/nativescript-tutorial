f1a1042771d9a004aa123cd1615faf6d59d92335
Amir Massarwi <amassarwi@gmail.com>
2017-06-20 15:21:41 +0200

    using code-behind file for the login page

---
+++ app/views/login/login.js
@@ -0,0 +1,9 @@
+exports.loaded = function() {
+    console.log("hello");
+};
+exports.signIn = function() {
+    alert("Signing in");
+};
+exports.register = function() {
+    alert("Registering");
+};
